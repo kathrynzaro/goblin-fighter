894361e99959c73a8488e26f73dcd6f579640197
kathrynzaro <zarokathryn@gmail.com>
2022-05-05 16:58:37 -0700

    click handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import { renderGoblin } from './utils.js';
 const form = document.getElementById('add-gob');
 const gobList = document.querySelector('.gobs');
+const defeatedEl = document.getElementById('defeated-num');
+const healthPoints = document.getElementById('player-hp');
 // let state
 let defeatedCount = 0;
 let playerHP = 11;
@@ -23,7 +25,7 @@ form.addEventListener('submit', (e) => {
         hp: Math.ceil(Math.random() * 7),
     };
     goblins.push(newGoblin);
-
+    form.reset();
     displayGoblins();
 });
 
@@ -32,7 +34,40 @@ function displayGoblins() {
     for (let goblin of goblins) {
         const goblinEl = renderGoblin(goblin);
 
+        goblinEl.addEventListener('click', () => {
+            goblinClickHandler(goblin);
+        });
+
         gobList.append(goblinEl);
+    }
+}
+
+function goblinClickHandler(goblin) {
+    console.log(`clicking ${goblin.name}`);
+    if (goblin.hp === 0) return;
+    if (playerHP === 0) return;
+    const playerHit = Math.random();
+    if (playerHit < .5) {
+        goblin.hp--;
+        displayGoblins();
+        alert(`You bludgeoned ${goblin.name}!`);
+        if (goblin.hp === 0) {
+            defeatedCount++;
+            defeatedEl.textContent = defeatedCount;
+        }
+    } else {
+        alert(`You missed ${goblin.name}, you fool!`);
+    }
+    const goblinHit = Math.random();
+    if (goblinHit < .5) {
+        playerHP--;
+        healthPoints.textContent = playerHP;
+        alert(`${goblin.name} has wounded you!`);
+        if (playerHP === 0) {
+            alert('GAME OVER');
+        }
+    } else {
+        alert(`Hoorah! You dodged ${goblin.name}!`)
     }
 }
 
